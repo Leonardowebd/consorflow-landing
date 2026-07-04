fd1a743f4c1673de475c531efaf6e7a4a01dd5d5
Leonardo Aragão <nleo17000@gmail.com>
2026-07-04 13:09:16 -0300

    site: instala Google Tag Manager GTM-PGTFC8HQ

--- blog/_tools/gerar_post.py
+++ blog/_tools/gerar_post.py
@@ -329,6 +329,9 @@ def render_post(p, site):
 <html lang="pt-BR">
 <head>
 <meta charset="UTF-8">
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){{w[l]=w[l]||[];w[l].push({{'gtm.start':new Date().getTime(),event:'gtm.js'}});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);}})(window,document,'script','dataLayer','GTM-PGTFC8HQ');</script>
+<!-- End Google Tag Manager -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>{esc(p["title"])} | Consorflow</title>
 <meta name="description" content="{esc(p["meta_description"])}">
@@ -355,6 +358,9 @@ def render_post(p, site):
 </script>
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PGTFC8HQ" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 {NAV}
   <div class="wrap"><p class="crumbs"><a href="/">Início</a> › <a href="/blog/">Blog</a> › {esc(p["title"][:40])}</p></div>
   <article class="post">
