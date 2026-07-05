a22006544e0cbb98481354dd865e725d11c5e60b
Leonardo Aragão <nleo17000@gmail.com>
2026-07-05 12:29:40 -0300

    site: instala Google tag GA4 G-6QC9WES51E

--- blog/_tools/gerar_post.py
+++ blog/_tools/gerar_post.py
@@ -364,6 +364,15 @@ def render_post(p, site):
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){{w[l]=w[l]||[];w[l].push({{'gtm.start':new Date().getTime(),event:'gtm.js'}});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);}})(window,document,'script','dataLayer','GTM-PGTFC8HQ');</script>
 <!-- End Google Tag Manager -->
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6QC9WES51E"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){{dataLayer.push(arguments);}}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6QC9WES51E');
+</script>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>{esc(p["title"])} | Consorflow</title>
 <meta name="description" content="{esc(p["meta_description"])}">
